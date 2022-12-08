3a4f7c2b259681860ff245b575a788af93078d43
Rumen Lishkov <airdata@abv.bg>
2022-12-09 00:51:54 +0200

    Add a companys

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const PORT = process.env.PORT || 5000
 http.createServer(function (request, response) {
     console.log('request ', request.url);
 
-    var filePath = './public' + request.url;
-    if (filePath == './public/') {
-        filePath = './public/index.html';
+    var filePath = './docs' + request.url;
+    if (filePath == './docs/') {
+        filePath = './docs/index.html';
     }
 
     var extname = String(path.extname(filePath)).toLowerCase();
